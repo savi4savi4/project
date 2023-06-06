b5d6a252e89e51b744aee42c76ede7355888c920
Ivan <example@gmail.com>
2023-06-06 19:38:21 +0300

    коммит где у сема 10 а не 100

--- preview/script.js
+++ preview/script.js
@@ -1,25 +1,25 @@
-function pow(x, n) {
-    let result = 1;
+// function pow(x, n) {
+//     let result = 1;
 
-    for (let i = 0; i < n; i++) {
-        result *= x;
-    }
+//     for (let i = 0; i < n; i++) {
+//         result *= x;
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-function pow(x, n) {
-    if (n === 1) {
-        return x;
-    } else {
-        return x * pow(x, n - 1);
-    }
-}
+// function pow(x, n) {
+//     if (n === 1) {
+//         return x;
+//     } else {
+//         return x * pow(x, n - 1);
+//     }
+// }
 
-pow(2, 1) // 2
-pow(2, 2) // 4
-pow(2, 3) // 8
-pow(2, 4) // 16
+// pow(2, 1) // 2
+// pow(2, 2) // 4
+// pow(2, 3) // 8
+// pow(2, 4) // 16
 
 let students = {
     js: [{
@@ -41,7 +41,7 @@ let students = {
         
         pro: [{
             name: 'Sam',
-            progress: 100
+            progress: 10
         }]
     }
 };
