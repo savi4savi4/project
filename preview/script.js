11ae001656af0cbe20ce29504ee664c87b1e7f90
Ivan <example@gmail.com>
2023-07-02 19:01:41 +0300

    допОсн 09

--- preview/script.js
+++ preview/script.js
@@ -1,59 +1,30 @@
 'use strict';
 
-const user = {
-    name: 'Alex',
-    surname: 'Smith',
-    birhday: '20/04/1993',
-    showMyPublicData: function () {
-        console.log(`${this.name} ${this.surname}`);
-    }
+const arr = ['Alex', 'Ann', 'Oleg', 'Alex'];
+
+function unique(arr) {
+    return Array.from(new Set(arr));
 }
-console.log(typeof (Object.keys(user)[0]));
 
-const userMap = new Map(Object.entries(user));
+console.log(unique(arr));
+// const set = new Set(arr);
 
-const newUserObj = Object.fromEntries(userMap);
-console.log(newUserObj);
+// set.add('Ivan');
+// set.add('Oleg');
 
-const shops = [
-    { rice: 500 },
-    { oil: 200 },
-    { bread: 50 }
-]
+// console.log(set);
 
-const budget = [5000, 15000, 25000];
+// set.delete(value);
+// set.has(value);
+// set.clear();
+// set.size;
 
-const map = new Map([
-    [{ paper: 400}, 8000]
-]);
+// for (let value of set) console.log(value);
+// set.forEach((value, valueAgaing, set) => {
+//     console.log(value, valueAgaing);
+// })
 
-shops.forEach((shop, i) => {
-    map.set(shop, budget[i]);
-})
-
-console.log(map);
-// console.log(map.get(shops[0]));
-// console.log(map.has(shops[0]));
-// map.delete(key);
-// map.clear();
-// map.size;
-//map.keys();
-
-// const goods = [];
-// for (let shop of map.keys()) {
-//     goods.push(Object.keys(shop)[0])
-// }
-// console.log(goods)
-
-// for (let price of map.values()) {
-//     console.log(price);
-// }
-
-// for (let [shop, price] of map.entries()) {
-//     console.log(price, shop);
-// }
-
-// map.forEach((value, key, map) => {
-//     console.log(key, value);
-// });
+// console.log(set.values());
+// console.log(set.keys());
+// console.log(set.entries());
 
